19491aa1fec2423d23a9b48d18a1627bac4a54fd
Bondjono19 <144017805+Bondjono19@users.noreply.github.com>
2025-11-27 19:45:16 +0800

    Bezier curves added

--- beziercurveevaluator.h
+++ beziercurveevaluator.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "CurveEvaluator.h"
-
+#include "mat.h"
+#include "vec.h"
 //using namespace std;
 
 class BezierCurveEvaluator : public CurveEvaluator
@@ -12,3 +13,14 @@ public:
 		const float& fAniLength,
 		const bool& bWrap) const;
 };
+static Vec4f multiplyRowVectorByMat4(const Vec4f& T, const Mat4f& M)
+{
+    Vec4f result;
+
+    result[0] = T[0] * M[0][0] + T[1] * M[1][0] + T[2] * M[2][0] + T[3] * M[3][0];
+    result[1] = T[0] * M[0][1] + T[1] * M[1][1] + T[2] * M[2][1] + T[3] * M[3][1];
+    result[2] = T[0] * M[0][2] + T[1] * M[1][2] + T[2] * M[2][2] + T[3] * M[3][2];
+    result[3] = T[0] * M[0][3] + T[1] * M[1][3] + T[2] * M[2][3] + T[3] * M[3][3];
+
+    return result;
+}
---
+++ bsplinecurveevaluator.cpp
@@ -0,0 +1,10 @@
+#include "BSplineCurveEvaluator.h"
+#include <assert.h>
+
+void BSplineCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
+	std::vector<Point>& ptvEvaluatedCurvePts,
+	const float& fAniLength,
+	const bool& bWrap) const
+{
+	
+}
